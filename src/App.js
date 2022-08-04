4fd32a20461d10977544aeee668b7e7e7a9d7a9b
leegyuseung <lgs4002@naver.com>
2022-08-05 02:24:55 +0900

    할 일 목록 UI 만들기

--- src/App.js
+++ src/App.js
@@ -2,12 +2,36 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
+  //버튼 스타일
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRedius: "50%",
+    cursor: "pointer",
+    float: "right",
+  };
+
+  //리스트 스타일
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none",
+    };
+  };
+
   render() {
     return (
       <div className="container">
         <div className="todoBlock">
           <div className="title">
             <h1>할 일 목록</h1>
+          </div>
+          <div style={this.getStyle()}>
+            <input type="checkbox" defaultChecked={false} />
+            공부하기
+            <button style={this.btnStyle}>X</button>
           </div>
         </div>
       </div>
