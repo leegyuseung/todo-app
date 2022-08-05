77aeaeb04a8f0040336a8c5d421fdd49352bd413
leegyuseung <lgs4002@naver.com>
2022-08-06 01:14:22 +0900

    할 일 추가하기

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ export default class App extends Component {
         completed: false,
       },
     ],
+    value: "",
   };
 
   //버튼 스타일
@@ -43,6 +44,27 @@ export default class App extends Component {
     this.setState({ todoData: newTodoData });
   };
 
+  // 할일 입력 input
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  // 폼 submit
+  handleSubmit = (e) => {
+    // form 안에 input을 전송할 때 페이지 리로드 막아준다.
+    e.preventDefault();
+
+    // 새로운 할일 데이터
+    let newTodo = {
+      id: Date.now(),
+      title: this.state.value,
+      completed: false,
+    };
+
+    // 원래 있던 할 일에 새로운 할 일 더해주기
+    this.setState({ todoData: [...this.state.todoData, newTodo] });
+  };
+
   render() {
     return (
       <div className="container">
@@ -62,6 +84,22 @@ export default class App extends Component {
               </button>
             </div>
           ))}
+          <form style={{ display: "flex" }} onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="value"
+              style={{ flex: "10", padding: "5px" }}
+              placeholder="해야 할 일을 입력하세요."
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <input
+              type="submit"
+              value="입력"
+              className="btn"
+              style={{ flex: "1" }}
+            />
+          </form>
         </div>
       </div>
     );
