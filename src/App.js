23ed6d436d863c4d93dcb288cfb5ea7c9bc18b11
leegyuseung <lgs4002@naver.com>
2022-08-09 18:41:13 +0900

    컴포넌트 분리하기

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
+import List from "./components/List";
 
 export default function App() {
   const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
-
-  //버튼 스타일
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRedius: "50%",
-    cursor: "pointer",
-    float: "right",
-  };
-
-  //리스트 스타일
-  const getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none",
-    };
-  };
-
-  // 삭제 함수
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter((data) => data.id !== id);
-    setTodoData(newTodoData);
-  };
 
   // 할일 입력 input
   const handleChange = (e) => {
@@ -52,35 +28,13 @@ export default function App() {
     setValue("");
   };
 
-  const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    setTodoData(newTodoData);
-  };
-
   return (
     <div className="container">
       <div className="todoBlock">
         <div className="title">
           <h1>할 일 목록</h1>
         </div>
-        {todoData.map((data) => (
-          <div style={getStyle(data.completed)} key={data.id}>
-            <input
-              type="checkbox"
-              defaultChecked={false}
-              onChange={() => handleCompleteChange(data.id)}
-            />
-            {data.title}
-            <button style={btnStyle} onClick={() => handleClick(data.id)}>
-              X
-            </button>
-          </div>
-        ))}
+        <List todoData={todoData} setTodoData={setTodoData} />
         <form style={{ display: "flex" }} onSubmit={handleSubmit}>
           <input
             type="text"
---
+++ src/components/List.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const List = ({ todoData, setTodoData }) => {
+  //버튼 스타일
+  const btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRedius: "50%",
+    cursor: "pointer",
+    float: "right",
+  };
+
+  //리스트 스타일
+  const getStyle = (completed) => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: completed ? "line-through" : "none",
+    };
+  };
+
+  // 삭제 함수
+  const handleClick = (id) => {
+    let newTodoData = todoData.filter((data) => data.id !== id);
+    setTodoData(newTodoData);
+  };
+
+  const handleCompleteChange = (id) => {
+    let newTodoData = todoData.map((data) => {
+      if (data.id === id) {
+        data.completed = !data.completed;
+      }
+      return data;
+    });
+    setTodoData(newTodoData);
+  };
+
+  return (
+    <div>
+      {todoData.map((data) => (
+        <div style={getStyle(data.completed)} key={data.id}>
+          <input
+            type="checkbox"
+            defaultChecked={false}
+            onChange={() => handleCompleteChange(data.id)}
+          />
+          {data.title}
+          <button style={btnStyle} onClick={() => handleClick(data.id)}>
+            X
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default List;
