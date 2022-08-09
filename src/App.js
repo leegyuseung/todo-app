66f1338431d6564d38c4044a3564fad540731b3f
leegyuseung <lgs4002@naver.com>
2022-08-09 19:09:16 +0900

    컴포넌트 분리하기2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
+import Form from "./components/Form";
 import List from "./components/List";
 
 export default function App() {
   const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
-
-  // 할일 입력 input
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
 
   // 폼 submit
   const handleSubmit = (e) => {
@@ -35,22 +31,7 @@ export default function App() {
           <h1>할 일 목록</h1>
         </div>
         <List todoData={todoData} setTodoData={setTodoData} />
-        <form style={{ display: "flex" }} onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="value"
-            style={{ flex: "10", padding: "5px" }}
-            placeholder="해야 할 일을 입력하세요."
-            value={value}
-            onChange={handleChange}
-          />
-          <input
-            type="submit"
-            value="입력"
-            className="btn"
-            style={{ flex: "1" }}
-          />
-        </form>
+        <Form value={value} setValue={setValue} handleSubmit={handleSubmit} />
       </div>
     </div>
   );
