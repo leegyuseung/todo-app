f3603c86ad900e0436cb4e5ea15a874511fbbe89
leegyuseung <lgs4002@naver.com>
2022-08-10 21:26:46 +0900

    List 컴포넌트 생성하기

---
+++ src/components/Lists.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import List from "./List";
+
+const Lists = ({ todoData, setTodoData }) => {
+  // dnd 순서정렬
+  const handleEnd = (result) => {
+    console.log("result", result);
+    //목적지가 없으면 종료
+    if (!result.destination) return;
+
+    //리액트 불변성을 지켜주기 위해 새로운 todoData 생성
+    const newTodoData = todoData;
+
+    // 1. 변경시키는 아이템을 배열에서 지우기
+    // 2. return 값으로 지워진 아이템을 잡아준다
+    const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+
+    //원하는 자리에 reorderItem을 insert 해줍니다.
+    newTodoData.splice(result.destination.index, 0, reorderedItem);
+    setTodoData(newTodoData);
+  };
+
+  return (
+    <div>
+      <DragDropContext onDragEnd={handleEnd}>
+        <Droppable droppableId="todo">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {todoData.map((data, index) => (
+                <Draggable
+                  key={data.id}
+                  draggableId={data.id.toString()}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <List
+                      key={data.id}
+                      id={data.id}
+                      title={data.title}
+                      completed={data.completed}
+                      todoData={todoData}
+                      setTodoData={setTodoData}
+                      provided={provided}
+                      snapshot={snapshot}
+                    />
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+};
+
+export default Lists;
