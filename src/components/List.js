7f1f7de83cfab1fd3a40395ee02f974a3f00af45
leegyuseung <lgs4002@naver.com>
2022-08-10 22:35:38 +0900

    할 일 목록을 수정하는 기능 추가

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const List = React.memo(
   ({
@@ -11,6 +11,9 @@ const List = React.memo(
     snapshot,
     handleClick,
   }) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedTitle, setEditedTitle] = useState(title);
+
     const handleCompleteChange = (id) => {
       let newTodoData = todoData.map((data) => {
         if (data.id === id) {
@@ -20,36 +23,93 @@ const List = React.memo(
       });
       setTodoData(newTodoData);
     };
-    return (
-      <div
-        key={id}
-        ref={provided.innerRef}
-        {...provided.draggableProps}
-        {...provided.dragHandleProps}
-        className={`${
-          snapshot.isDragging ? "bg-gray-100" : "bg-gray-100"
-        } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}
-      >
-        <div className="items-center">
-          <input
-            type="checkbox"
-            defaultChecked={completed}
-            onChange={() => handleCompleteChange(id)}
-          />
-          <span className={completed ? "line-through" : undefined}>
-            {title}
-          </span>
+
+    // 수정내용 보여주기
+    const handleEditChange = (e) => {
+      setEditedTitle(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      let newTodoData = todoData.map((data) => {
+        if (data.id === id) {
+          data.title = editedTitle;
+        }
+        return data;
+      });
+      setTodoData(newTodoData);
+      setIsEditing(false);
+    };
+
+    if (isEditing) {
+      return (
+        <div
+          className={`flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded bg-gray-100`}
+        >
+          <div className="items-center">
+            <form onSubmit={handleSubmit}>
+              <input
+                value={editedTitle}
+                onChange={handleEditChange}
+                className="w-full px-3 py-2 mr-4 text-gray-500 rounded"
+              />
+            </form>
+          </div>
+          <div className="items-center">
+            <button
+              className="px-4 py-2 float-right"
+              onClick={() => setIsEditing(false)}
+            >
+              X
+            </button>
+            <button
+              onClick={handleSubmit}
+              className="px-4 py-2 float-right"
+              type="submit"
+            >
+              save
+            </button>
+          </div>
         </div>
-        <div className="items-center">
-          <button
-            className="px-4 py-2 float-right"
-            onClick={() => handleClick(id)}
-          >
-            X
-          </button>
+      );
+    } else {
+      return (
+        <div
+          key={id}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          className={`${
+            snapshot.isDragging ? "bg-gray-100" : "bg-gray-100"
+          } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}
+        >
+          <div className="items-center">
+            <input
+              type="checkbox"
+              defaultChecked={completed}
+              onChange={() => handleCompleteChange(id)}
+            />
+            <span className={completed ? "line-through" : undefined}>
+              {title}
+            </span>
+          </div>
+          <div className="items-center">
+            <button
+              className="px-4 py-2 float-right"
+              onClick={() => handleClick(id)}
+            >
+              X
+            </button>
+            <button
+              className="px-4 py-2 float-right"
+              onClick={() => setIsEditing(true)}
+            >
+              edit
+            </button>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 );
 
